44e432ddb384e23a3b48f52c943bc2d9c49a277b
priyankabansalit07 <33718962+priyankabansalit07@users.noreply.github.com>
2021-04-21 19:34:00 +1000

    end of section 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -34,6 +34,13 @@ namespace API
             {
                 opt.UseSqlServer(_config.GetConnectionString("DefaultConnection"));
             });
+            services.AddCors(opt =>
+            {
+                opt.AddPolicy("CorsPolicy", policy =>
+                 {
+                     policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
+                 });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -49,6 +56,8 @@ namespace API
             // app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors("CorsPolicy");
 
             app.UseAuthorization();
 
---
+++ client-app/src/App.tsx
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import axios from 'axios';
+import { Header, List } from 'semantic-ui-react';
+
+function App() {
+
+  //hooks
+  const [activities, setActivities] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:5000/api/activities').then(response => {
+      console.log(response);
+      setActivities(response.data);
+    });
+  }, []); // last square bracket is added to prevent component to go into infinity loop.
+
+
+
+  return (
+    <div >
+      <Header as='h2' icon='users' content='Reactivties'/>
+        <List>  
+        {/* these are semantic ui react tags for design */}
+          {activities.map((activity: any) => (
+            <List.Item key={activity.id}>
+              {activity.title}
+            </List.Item>
+          ))}
+        </List>
+    </div>
+  );
+}
+
+export default App;
